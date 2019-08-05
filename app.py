afc579d5c65c67a433dd55d62756b960f7d3653b
Megan Leet <megan.leet@iinet.net.au>
2019-08-05 17:33:51 +1000

    Reorganize everything

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ app = Bottle()
 
 @app.route('/')
 def index():
+	return template('main_page')
+
+@app.route('/character_matchups/main')
+def character_matchups():
 	#I should use IDs...
 	regions = {region['Short']: region['Name'] for region in ausmash_lib.get_regions()}
 	games = {game['Short']: game['Name'] for game in ausmash_lib.get_games()}
-	return template('main_form', regions=regions, games=games)
+	return template('character_matchups_main', regions=regions, games=games)
 
-@app.route('/main')
-def get_stuff():
+@app.route('/character_matchups/results')
+def character_matchup_results():
 	region = request.query.region
 	player = request.query.player
 	game = request.query.game
@@ -23,9 +27,9 @@ def get_stuff():
 	try:
 		player_id = ausmash_lib.get_player(region, player)['ID']
 	except HTTPError:
-		return template('error', region=region, name=player)
+		return template('character_matchups_error', region=region, name=player)
 	
 	scores = ausmash_lib.group_player_score_against_characters(player_id, game)
-	return template('scores_view', region=region, player=player, game=game, scores=scores)
+	return template('character_matchups_results', region=region, player=player, game=game, scores=scores)
 
 run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
---
+++ main_page.tpl
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Ausmash Tools</title>
+	</head>
+	<body>
+		<p>
+			This page is where <a href="https://twitter.com/Zowayix">Zowayix</a> puts her random crap that uses the <a href="https://ausmash.com.au/">Ausmash</a> API for various messing around with of things. Source for stuff is on <a href="https://github.com/Zowayix/ausmash-tools">Github</a> in case it becomes useful to anyone else.
+		</p>
+		<p>
+			Currently this exists:
+			<li>
+				<ul><a href="/character_matchups/main">Opponent character matchups</a>: View results against opposing characters</ul>
+			</li>
+		</p>
+	</body>
+</html>
